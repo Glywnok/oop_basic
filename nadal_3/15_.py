2cd4954b3d047551f35083bedb274d94ecbf1f6a
Glywnok <arturlohatin@gmail.com>
2022-02-02 13:45:57 +0200

    Tehtud, õpetaja kontrollis.

--- nadal_3/15_.py
+++ nadal_3/15_.py
@@ -1,17 +1,31 @@
 from nadal_3.Kasutaja import Kasutaja
+import time
 password = "qwerty"
-nimi = str(input(("Tere!\n Palun sisestage oma nimet: ")))
-perenimi = str(input("Sisestage oma perekonna nimet: "))
+nimi = str(input(("Tere!\nPalun sisestage oma kasutaja nimet: ")))
 
-print("Teie paroolon juba määratud, kui te tahate teda muuta palun sisestage järgmised andmed: ")
+print("Teie parool on juba määratud, kui te tahate teda muuta palun sisestage järgmised andmed: ")
 
 katsed_suurendamine = int(input("On võimalik, et te ei mäleta oma parooli, ja te tahate suurendada parooli sisselogimis katseid: siis kui palju te tahate et see arv suurenes? "))
 katsed = int(input("Maksimaalne arv katse: "))
+print("Te olete sisetanud andmed mis on vaja, nüüd oodake.")
+time.sleep(4)
+Kasutaja1 = Kasutaja(nimi, katsed_suurendamine, katsed, password)
 
-Kasutaja1 = Kasutaja(nimi, perenimi, katsed_suurendamine, katsed, password)
+setup = Kasutaja1.password_test()
+time.sleep(2)
+print("Nüüd te võite vahetata oma parooli, kui tahate. ")
+time.sleep(2)
+ask1 = input(str("Kas te tahate vahetada oma parooli? Jah/jah või Ei/ei "))
 
-Kasutaja1.password_test()
-print(password)
+if ask1 == "Jah" or "jah":
+    new_password = Kasutaja1.NEW_PASS(setup)
+    Kasutaja1.set_password(new_password)
 
-Kasutaja2 = Kasutaja(nimi, perenimi, katsed_suurendamine, katsed, password)
 
+print("Nüüd vaatame teie parooli ja kasutajanimele!")
+
+time.sleep(2.5)
+Kasutaja1.kirjelda_kasutajat()
+time.sleep(2)
+print("Tere " + nimi + "!")
+time.sleep(1.5)
--- nadal_3/Kasutaja.py
+++ nadal_3/Kasutaja.py
@@ -1,8 +1,9 @@
+import time
 class Kasutaja():
 
-    def __init__(self, eesnimi, perenimi, suurenda, sisselogimiskatsed, password):
-        self.eesnimi = eesnimi
-        self.perenimi = perenimi
+
+    def __init__(self, nickname, suurenda, sisselogimiskatsed, password):
+        self.nickname = nickname
         self.suurenda_sisselogimiskatsed = suurenda
         self.sisselogimiskatsed = sisselogimiskatsed
         self.times = 0
@@ -10,29 +11,61 @@ class Kasutaja():
 
     def kirjelda_kasutajat(self):
         print("Kasutaja andmed: ")
-        print("Kasutaja eesnimi on " + str(self.eesnimi))
-        print("Kasutaja perekonnanimi on " + str(self.perenimi))
+        print("Kasutaja: " + self.nickname)
+        print("Kasutaja parool on: " + self.password)
 
     def tervitaja_kasutajat(self):
-        print("Tere " + str(self.eesnimi) + " " + str(self.perenimi))
+        print("Tere " + str(self.nickname))
 
     def password_test(self):
-        ask = str(input("Sisestage palun sisse oma parooli: "))
-        if ask == self.password:
-            print("Sisestatud parool on õige")
+        while self.times <= self.sisselogimiskatsed:
+            ask = str(input("Sisestage palun sisse oma parooli: "))
+            if ask == self.password:
+                print("Sisestatud parool on õige")
+                setup = True
+                break
+            else:
+                print("Parool on vale")
+                self.times += 1
+                if self.times == self.sisselogimiskatsed:
+                    print("Te olete sisestatud valesti parooli juba " + str(self.times) + " te ei saa rohkem sisestada parooli.")
+                    self.times = 0
+                    exit()
+                setup = False
+        return setup
+
+    def kontroll_password(self, password):
+        if len(password) < 6:
+            print("Uus parool on liiga lühike. ")
+            return False
         else:
-            print("Parool on vale")
-            self.times += 1
-            if self.times == self.sisselogimiskatsed:
-                print("Te olete sisestatud valesti parooli juba " + str(
-                    self.times) + " te ei saa rohkem sisestada parooli.")
-                self.times = 0
-                exit()
+            if len(password) > 10:
+                print("Uus parool on liiga pikk.")
+                return False
+            else:
+                return True
 
-    def setter(self):
-        muuda = str(input("Te sisestasite õigesti parooli! Kas te tahate teda muutuda? jah/ei"))
-        if muuda == "jah" or "Jah":
-            password = str(input("Sisestage oma parooli"))
-        return password
 
-    def getter(self):
+    def set_password(self, new_password):
+        if self.kontroll_password(new_password):
+            self.password = new_password
+
+    def NEW_PASS(self, setup):
+        if setup == True:
+            sit = False
+            while sit == 0:
+                Password = input("Sisestage palun oma parool: ")
+                Password_try = input("Sisestage palun see sama parool: ")
+                if Password == Password_try:
+                    sit = 1
+                    new_password = Password
+                else:
+                    print("Sisestatud parool on vale, proovige uuesti!")
+                    time.sleep(3)
+        return new_password
+
+
+
+    def get_password(self):
+        #print("Teie parool on: " + self.password)
+        return self.password
