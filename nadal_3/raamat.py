15a056d4a28bd4dc43683d4392c6f6f4bf993ac8
Glywnok <arturlohatin@gmail.com>
2022-02-08 11:53:09 +0200

    Need to finish

---
+++ nadal_3/raamat.py
@@ -0,0 +1,7 @@
+class raamat():
+
+    def __init__(self, pealkiri, autor, hind, reiting):
+        self.pealkiri = pealkiri
+        self.autor = autor
+        self.hind = hind
+        self.reiting = reiting
---
+++ nadal_3/raamatupood.py
@@ -0,0 +1,34 @@
+#from raamat import raamat
+
+class raamatupood():
+    def __init__(self, nimi, reiting):
+        pass
+
+    def saan_lisada_raamat(self, raamat):
+        pass
+
+'''
+    def lisa_raamat(self, raamat: Raamat):
+        pass
+
+    def saan_eemaldada_raamat(self, raamat: Raamat):
+        pass
+
+    def eemalda_raamat(self, raamat: Raamat):
+        pass
+
+    def naita_koik_raamatud(self):
+        pass
+
+    def naita_raamatud_hinna_jargi(self):
+        pass
+
+    def naita_koige_populaarsem_raamat(self):
+        pass
+        '''
+
+'''
+minu_raamat = raamat('naksitralid', 'raud', '10', '8.5')
+pood = raamatupood('minu pood', 10)
+pood.saan_lisada_raamat(minu_raamat)
+'''
